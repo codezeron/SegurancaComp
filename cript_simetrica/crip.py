e4afd5b8da03eee0d36f77efa1f7cc16d7f250cf
codezeron <enzoyutakah@gmail.com>
2023-08-23 19:34:42 -0300

    ✨ - feat: separados func decrypt bin e txt, print todos as etapas do crypt

--- cript_simetrica/crip.py
+++ cript_simetrica/crip.py
@@ -8,8 +8,13 @@ def encrypt_des(data, key):
     encrypted_data = cipher.encrypt(pad(data, DES.block_size))  # Criptografa os dados após preenchimento
     return encrypted_data
 
-# Função para descriptografar dados usando DES
 def decrypt_des(encrypted_data, key):
+    cipher = DES.new(key, DES.MODE_ECB)  # Cria um objeto DES com a chave e o modo ECB
+    decrypted_data = unpad(cipher.decrypt(encrypted_data), DES.block_size)  # Descriptografa e remove o preenchimento
+    return decrypted_data.decode() 
+
+# Função para descriptografar arquivos binarios usando DES
+def decrypt_des_bin(encrypted_data, key):
     cipher = DES.new(key, DES.MODE_ECB)  # Cria um objeto DES com a chave e o modo ECB
     decrypted_data = cipher.decrypt(encrypted_data)  # Descriptografa e remove o preenchimento
     return decrypted_data  
@@ -31,12 +36,18 @@ def main():
         if choice == "1":
             plaintext = "Texto exemplo para criptografar"  # Texto de exemplo para criptografar
             encrypted_data = encrypt_des(plaintext.encode(), key)  # Chama a função de criptografia
+            decrypted_data = decrypt_des(encrypted_data, key)  # Chama a função de descriptografia
+            print("Texto original:", plaintext)
             print("Texto criptografado:", encrypted_data)  # Exibe o texto criptografado
+            print("Texto descriptografado:", decrypted_data)
 
         elif choice == "2":
             plaintext = input("Digite o texto para criptografar: ")  # Recebe o texto a ser criptografado
             encrypted_data = encrypt_des(plaintext.encode(), key)  # Chama a função de criptografia
+            decrypted_data = decrypt_des(encrypted_data, key)  # Chama a função de descriptografia
+            print("Texto original:", plaintext)
             print("Texto criptografado:", encrypted_data)  # Exibe o texto criptografado
+            print("Texto descriptografado:", decrypted_data)
 
         elif choice == "3":
             file_name = input("Digite o nome do arquivo de texto: ")  # Recebe o nome do arquivo de texto
@@ -46,6 +57,9 @@ def main():
                 encrypted_file_name = file_name + ".enc"  # Gera o nome do arquivo criptografado
                 with open(encrypted_file_name, "wb") as encrypted_file:
                     encrypted_file.write(encrypted_data)  # Salva os dados criptografados no arquivo
+                print("Texto original:", plaintext)
+                print("Texto criptografado:", encrypted_data)
+                print("Texto descriptografado:", decrypted_data)
                 print("Arquivo criptografado salvo como:", encrypted_file_name)  # Exibe o nome do arquivo criptografado
 
         elif choice == "4":
@@ -62,7 +76,7 @@ def main():
             encrypted_file_name = input("Digite o nome do arquivo criptografado: ")
             with open(encrypted_file_name, "rb") as encrypted_file:
                 encrypted_data = encrypted_file.read()
-                decrypted_data = decrypt_des(encrypted_data, key)
+                decrypted_data = decrypt_des_bin(encrypted_data, key)
                 decrypted_file_name = encrypted_file_name[:-4]  # Remove a extensão ".enc"
                 with open(decrypted_file_name, "wb") as decrypted_file:
                     decrypted_file.write(decrypted_data)
