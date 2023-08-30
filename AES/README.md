b67e2d68cedbeb141123662429fb2285060fc31e
codezeron <enzoyutakah@gmail.com>
2023-08-30 19:11:11 -0300

    ✨ - feat: add readme

---
+++ AES/README.md
@@ -0,0 +1,24 @@
+
+
+## Trabalho 2
+
+Criação de um algoritmo de criptografia simétrica utilizando a linguagem que preferirem.
+Reutilizar seu código feito na última atividade para que permita o uso do algoritmo de criptografia AES. Um exemplo básico está em anexo.
+Seu código deve dar a opção para utilizar chaves de 128 e 256 bits e funcionar com todos os modos de operação
+
+Atividade com AES e modos de operação
+
+
+Requisito:
+Instalar a biblioteca pycryptodome v3.18.0 ou acima
+
+Para testar o trabalho:
+
+1. inicie o arquivo na pasta AES
+
+2. inicie o código com `python3 aes.py`
+
+3. insira uma o número pedido (128 ou 256)
+
+Os resultados esperados são a saída textos criptografados e descriptografados na tela  
+
--- AES/aes.py
+++ AES/aes.py
@@ -1,11 +1,14 @@
+
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
+
 
 def encrypt_aes(data, key, mode):
     cipher = AES.new(key, mode)
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
     return encrypted_data
+
 
 def decrypt_aes(encrypted_data, key, mode):
     cipher = AES.new(key, mode)
@@ -15,9 +18,10 @@ def decrypt_aes(encrypted_data, key, mode):
         decrypted_data = cipher.decrypt(encrypted_data)
     return decrypted_data
 
+
 def main():
     key_length = int(input("Escolha o tamanho da chave (128 ou 256): "))
-    if key_length != 128 and key_length != 256:
+    if key_length not in (128, 256):
         print("Tamanho de chave inválido.")
         return
 
@@ -25,7 +29,10 @@ def main():
 
     data = b"Texto para criptografar e descriptografar"
 
-    modes = [AES.MODE_ECB, AES.MODE_CBC, AES.MODE_CFB, AES.MODE_OFB, AES.MODE_CTR]
+    modes = [
+            AES.MODE_ECB, AES.MODE_CBC, AES.MODE_CFB,
+            AES.MODE_OFB, AES.MODE_CTR
+            ]
     mode_names = ["ECB", "CBC", "CFB", "OFB", "CTR"]
 
     for mode, mode_name in zip(modes, mode_names):
@@ -37,6 +44,7 @@ def main():
         print("Texto criptografado:", encrypted_data)
         print("Texto descriptografado:", decrypted_data)
         print()
+
 
 if __name__ == "__main__":
     main()
